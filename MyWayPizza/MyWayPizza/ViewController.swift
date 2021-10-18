58f0d712341935f3eb93c79c04fa09c6c0f7ce04
Egzon <docpllana@gmail.com>
2021-10-18 20:09:01 +0200

    First commit - Push project to GitHub.

---
+++ MyWayPizza/MyWayPizza/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  MyWayPizza
+//
+//  Created by Egzon Pllana on 18.10.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
